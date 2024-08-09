f7c969bf0386a28e870c32c9166d49e9a5e53297
uppuramu <uppuramu7268@gmail.com>
2024-08-09 17:15:32 +0530

    second

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+
 
 # Set the title of the Streamlit app
 st.title("Exploratory Data Analysis (EDA) App with Interactive Buttons")
